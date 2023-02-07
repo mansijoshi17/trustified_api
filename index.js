dea7a72d27e287199700b74ca43cbafc25d816d9
mansijoshi17 <mansijoshi1777@gmail.com>
2023-02-07 12:25:55 +0530

    Changes in index

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ require("dotenv").config();
 const port = 8000;
 
 const router = require("./routes/index");
+const jsFile = require("./widget/trustified");
+const cssFile = require("./widget/index.css");
 
 app.use(logger("dev"));
 app.use(cors());
@@ -16,6 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/trustified/api", router);
+app.use("/trustified/loginwithtrustified", jsFile);
+app.use("/trustified/css", cssFile);
 
 app.listen(port, function () {
   console.log("Runnning on " + port);
